e71c5e3ec3f2e2d0706db9b9dc1228614dadba0a
Lauréline <laureline.drieu@gmail.com>
2023-10-23 17:00:23 +0200

    commit 3 bouton animation

--- app/src/main/java/com/example/exemple_template/HomeActivity.java
+++ app/src/main/java/com/example/exemple_template/HomeActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.exemple_template.a01_layout.L01_linearActivity;
 import com.example.exemple_template.a01_layout.L02_frameActivity;
 import com.example.exemple_template.a01_layout.L03_relativeActivity;
+import com.example.exemple_template.a03_animation.a01_loadingActivity;
+import com.example.exemple_template.a03_animation.a02_simpsonActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -22,12 +25,16 @@ public class HomeActivity extends AppCompatActivity {
     Button btn_linear_layout ;
     Button btn_frame_layout;
     Button btn_relative_layout;
+    Button btn_animation_loading;
+    Button btn_animation_simpson;
 
     // 2 Méthode init pour l'initialisation des widgets
     public  void  initUI(){
         btn_linear_layout = findViewById(R.id.btn_linear_layout);
         btn_frame_layout = findViewById(R.id.btn_frame_layout);
         btn_relative_layout = findViewById(R.id.btn_relative_layout);
+        btn_animation_loading = findViewById(R.id.btn_animation_loading);
+        btn_animation_simpson = findViewById(R.id.btn_animation_simpson);
     }
 
     // 4 Méthode pour la gestion du bouton LinearLayout
@@ -58,6 +65,28 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    public void setBtn_animation_loading(){
+        btn_animation_loading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, a01_loadingActivity.class));
+            }
+        });
+
+        btn_animation_loading.animate();
+
+    }
+
+    public void setBtn_animation_simpson(){
+        btn_animation_simpson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, a02_simpsonActivity.class));
+            }
+        });
+
+    }
+
     /*
     Life Cycles
      */
@@ -71,5 +100,8 @@ public class HomeActivity extends AppCompatActivity {
         setBtn_linear_layout(); //5 Appel de la Méthode
         setBtn_frame_layout();
         setBtn_relative_layout();
+        setBtn_animation_loading();
+        setBtn_animation_simpson();
+
     }
 }
